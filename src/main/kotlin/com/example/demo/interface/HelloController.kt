18c90f9cbaee03236c8cc0a24ed4d4b1ce542a63
simple <shwncho4@gmail.com>
2024-07-23 22:30:00 +0900

    hello world

---
+++ src/main/kotlin/com/example/demo/interface/HelloController.kt
@@ -0,0 +1,15 @@
+package com.example.demo.`interface`
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(model: Model): String{
+        model.addAttribute("message", "Hello World")
+        return "hello"
+    }
+}
