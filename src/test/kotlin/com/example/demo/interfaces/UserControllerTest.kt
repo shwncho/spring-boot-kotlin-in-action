cd816860096882f878325c3a3aed55331ccf2b82
shwncho <shwncho4@gmail.com>
2024-08-26 19:57:13 +0900

    test: findUser

--- src/test/kotlin/com/example/demo/interfaces/UserControllerTest.kt
+++ src/test/kotlin/com/example/demo/interfaces/UserControllerTest.kt
@@ -1,5 +1,6 @@
 package com.example.demo.interfaces
 
+import com.example.demo.application.UserResponse
 import com.example.demo.application.UserService
 import com.example.demo.interfaces.request.UserRequestDto
 import com.example.demo.support.RestDocsTest
@@ -10,14 +11,14 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
 import io.restassured.http.ContentType
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.payload.JsonFieldType
-import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
-import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.restdocs.payload.PayloadDocumentation.*
+import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
+import org.springframework.restdocs.request.RequestDocumentation.pathParameters
 
 class UserControllerTest: RestDocsTest() {
     private lateinit var userService: UserService
@@ -46,6 +47,33 @@ class UserControllerTest: RestDocsTest() {
                     requestPreprocessor(),
                     responsePreprocessor(),
                     requestFields(
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("user name"),
+                        fieldWithPath("age").type(JsonFieldType.NUMBER).description("user age")
+                    )
+                )
+            )
+    }
+
+    @Test
+    fun findUser(){
+        every{ userService.find(any()) } returns UserResponse(1L,"simple",0)
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("id",1L)
+            .get("/api/v1/users/{id}")
+            .then()
+            .status(HttpStatus.OK)
+            .apply(
+                document(
+                    "findUser",
+                    requestPreprocessor(),
+                    responsePreprocessor(),
+                    pathParameters(
+                        parameterWithName("id").description("user id"),
+                    ),
+                    responseFields(
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("user id"),
                         fieldWithPath("name").type(JsonFieldType.STRING).description("user name"),
                         fieldWithPath("age").type(JsonFieldType.NUMBER).description("user age")
                     )
