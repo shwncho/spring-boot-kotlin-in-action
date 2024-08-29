d1dab159dd2ed9a77ee8c8e416df89bbad7cf403
shwncho <shwncho4@gmail.com>
2024-08-29 14:41:09 +0900

    test: user find

--- src/test/kotlin/com/example/demo/application/UserServiceTest.kt
+++ src/test/kotlin/com/example/demo/application/UserServiceTest.kt
@@ -3,10 +3,16 @@ package com.example.demo.application
 import com.example.demo.domain.User
 import com.example.demo.domain.UserRepository
 import io.kotest.assertions.any
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.*
+import io.mockk.junit5.MockKExtension
+import jakarta.persistence.EntityNotFoundException
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.data.repository.findByIdOrNull
 
+@ExtendWith(MockKExtension::class)
 class UserServiceTest: BehaviorSpec({
     val userRepository = mockk<UserRepository>()
     val userService = UserService(userRepository)
@@ -22,6 +28,31 @@ class UserServiceTest: BehaviorSpec({
                 verify(exactly = 1) { userRepository.save(any()) }
                 savedUser.name shouldBe "simple"
                 savedUser.age shouldBe 0
+            }
+        }
+    }
+
+    Given("유저의 id가 주어지는 경우"){
+        val id = 1L
+
+        When("존재하는 유저를 조회하면"){
+            val name = "simple"
+            val age = 0
+            every { userRepository.findByIdOrNull(id) } returns User(name = name, age = age, id = id)
+
+            val userResponse = userService.find(id)
+            Then("유저를 정보를 리턴한다."){
+                verify(exactly = 1) { userRepository.findByIdOrNull(id) }
+                userResponse.id shouldBe id
+            }
+        }
+
+        When("존재하지 않는 유저를 조회하면"){
+            every{ userRepository.findByIdOrNull(any()) }   returns null
+
+            val exception = shouldThrow<EntityNotFoundException> { userService.find(id) }
+            Then("예외를 발생시킨다."){
+                exception.message shouldBe "존재하지 않는 유저 입니다."
             }
         }
     }
